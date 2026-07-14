2e8a8ba11692d90336e399660ed515be57f4a986
Richard Conway <richard@rconway.co.uk>
2026-07-14 17:45:29 +0100

    Add BrotherLaser printer

--- configuration/printing.nix
+++ configuration/printing.nix
@@ -5,4 +5,18 @@
   services.printing.enable = true;
   # But disable printer auto-discovery.
   systemd.services.cups-browsed.enable = false;
+
+  hardware.printers.ensurePrinters = [
+    {
+      name = "BrotherLaser";
+      description = "Brother HL-L3230CDW";
+      location = "Office";
+      deviceUri = "ipp://192.168.0.11";
+      model = "everywhere";
+      ppdOptions = {
+        PageSize = "A4";
+        Duplex = "DuplexNoTumble";
+      };
+    }
+  ];
 }
