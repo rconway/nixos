1e17492e338b0918e3a7436ee858c57c2349ef10
Richard Conway <richard@rconway.co.uk>
2026-08-30 20:45:27 +0100

    [nixos3268] make some tidy-up

--- hosts/nixos3268.nix
+++ hosts/nixos3268.nix
@@ -15,8 +15,6 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos3268";
 
